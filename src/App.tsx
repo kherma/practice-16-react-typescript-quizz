df24465d79bcc5937aeab7b6a871764e93787b88
kherma <kamil.herma01@gmail.com>
2021-03-28 04:50:08 +0200

    setup api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 
 import QuestoinCart from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+import { QuestionState, Difficulty } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const App = () => {
   const TOTAL_QUESTIONS = 10;
+
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [questionNumber, setQuestionNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrivia = async () => {};
   const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = () => {};
@@ -19,14 +32,14 @@ const App = () => {
       <button onClick={startTrivia}>Start</button>
       <p className="score">Score: </p>
       <p>Loading Questions...</p>
-      <QuestoinCart
+      {/* <QuestoinCart
         questionNr={questionNumber + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[questionNumber].question}
         answers={questions[questionNumber].answers}
         userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
